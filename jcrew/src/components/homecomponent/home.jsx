05a0a1748da6fc72467e7f9afcd3f01b3fa733dc
md-sahil-a <mdsahilamaan2017@gmail.com>
2022-08-27 19:52:14 +0530

    update

---
+++ jcrew/src/components/homecomponent/home.jsx
@@ -0,0 +1,28 @@
+import "./home.css";
+const Home = () => {
+  return (
+    <div className="nav">
+      <div>
+        <p>HOme</p>
+      </div>
+      <div><p>cart</p></div>
+    </div>
+
+  );
+};
+export default Home;
+
+
+
+{/* <div class="navbar">
+<a href="#home">Home</a>
+<a href="#news">News</a>
+<div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
+</div> */}
