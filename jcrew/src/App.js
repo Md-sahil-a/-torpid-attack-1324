e873a1252ad00a67830427a5c7530f0b091f1b6f
md-sahil-a <mdsahilamaan2017@gmail.com>
2022-08-28 15:25:44 +0530

    update

--- jcrew/src/App.js
+++ jcrew/src/App.js
@@ -1,5 +1,6 @@
-// import Home from "./components/homecomponent/home.jsx";
+import Home from "./components/homecomponent/home.jsx";
 // import { ManPage } from "./components/manpages/manpage.jsx";
+import { ManInnerPage } from "./components/manpages/man.innerPage";
 import { Woman } from "./components/WomanPage/womanPage";
 
 const App = () => {
@@ -7,7 +8,8 @@ const App = () => {
     <>
       {/* <Home></Home> */}
       {/* <ManPage/> */}
-      <Woman/>
+      {/* <Woman/> */}
+      <ManInnerPage/>
     </>
   );
 };
